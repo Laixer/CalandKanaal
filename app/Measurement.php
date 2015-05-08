73fcb76c39f7992586b6cc8c99d5faf2fde657d3
yorickdewid <yorick17@outlook.com>
2015-05-08 18:48:56 -0400

    Simple models

---
+++ app/Measurement.php
@@ -0,0 +1,45 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Measurement extends Model
+{
+
+	protected $fillable = [
+		'recording_date',
+		'message',
+		'vecwsm_1_0',
+		'vecwsm_1_1',
+		'vecwsm_2_0',
+		'vecwsm_2_1',
+		'vecwsm_3_0',
+		'vecwsm_3_1',
+		'vecwsm_4_0',
+		'vecwsm_4_1',
+		'vecwsm_5_0',
+		'vecwsm_5_1',
+		'vecwsm_6_0',
+		'vecwsm_6_1',
+		'vecwsm_7_0',
+		'vecwsm_7_1',
+		'vecwsm_8_0',
+		'vecwsm_8_1',
+		'vecwsm_9_0',
+		'vecwsm_9_1',
+		'vecwsm_10_0',
+		'vecwsm_10_1',
+		'vecwsm_11_0',
+		'vecwsm_11_1',
+		'vecwsm_12_0',
+		'vecwsm_12_1',
+		'vecwsm_13_0',
+		'vecwsm_13_1',
+		'vecwsm_14_0',
+		'vecwsm_14_1',
+		'vecwsm_15_0',
+		'vecwsm_15_1',
+		'vecwsm_16_0',
+		'vecwsm_16_1'
+	];
+
+}
---
+++ app/Sensor.php
@@ -0,0 +1,45 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sensor extends Model
+{
+
+	protected $fillable = [
+		'measurement_time',
+		'message',
+		'val_1_0',
+		'val_1_1',
+		'val_2_0',
+		'val_2_1',
+		'val_3_0',
+		'val_3_1',
+		'val_4_0',
+		'val_4_1',
+		'val_5_0',
+		'val_5_1',
+		'val_6_0',
+		'val_6_1',
+		'val_7_0',
+		'val_7_1',
+		'val_8_0',
+		'val_8_1',
+		'val_9_0',
+		'val_9_1',
+		'val_10_0',
+		'val_10_1',
+		'val_11_0',
+		'val_11_1',
+		'val_12_0',
+		'val_12_1',
+		'val_13_0',
+		'val_13_1',
+		'val_14_0',
+		'val_14_1',
+		'val_15_0',
+		'val_15_1',
+		'val_16_0',
+		'val_16_1'
+	];
+
+}
